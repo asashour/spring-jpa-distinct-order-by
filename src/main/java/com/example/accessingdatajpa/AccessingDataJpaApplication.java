336cb11e6301619c341145f4b233a9cc256556b3
Ahmed Ashour <asashour@yahoo.com>
2025-08-14 15:52:45 +0200

    The failing scenario.

--- src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
+++ src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 
 @SpringBootApplication
 public class AccessingDataJpaApplication {
@@ -19,35 +22,20 @@ public class AccessingDataJpaApplication {
 	@Bean
 	public CommandLineRunner demo(CustomerRepository repository) {
 		return (args) -> {
-			// save a few customers
 			repository.save(new Customer("Jack", "Bauer"));
 			repository.save(new Customer("Chloe", "O'Brian"));
 			repository.save(new Customer("Kim", "Bauer"));
 			repository.save(new Customer("David", "Palmer"));
 			repository.save(new Customer("Michelle", "Dessler"));
 
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			repository.findAll().forEach(customer -> {
-				log.info(customer.toString());
-			});
-			log.info("");
-
-			// fetch an individual customer by ID
-			Customer customer = repository.findById(1L);
-			log.info("Customer found with findById(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
-
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			repository.findByLastName("Bauer").forEach(bauer -> {
-				log.info(bauer.toString());
-			});
-			log.info("");
+			Specification<Customer> spec = (root, query, cb) -> {
+				if (query != null) {
+					query.distinct(true);
+				}
+				return cb.conjunction();
+			};
+			var pageable = PageRequest.of(0, 10, Sort.by(Sort.Order.by("firstName").ignoreCase()));
+			repository.findAll(spec, pageable);
 		};
 	}
 
--- src/main/java/com/example/accessingdatajpa/CustomerRepository.java
+++ src/main/java/com/example/accessingdatajpa/CustomerRepository.java
@@ -2,9 +2,10 @@ package com.example.accessingdatajpa;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
 
 	List<Customer> findByLastName(String lastName);
 
